4f06f4b74a55f5902a6e6b7d0f25c433d65180d6
caiyou <rodickcai@gmail.com>
2018-04-17 18:37:41 +0800

    UPDATE class实现

--- src/swiper-animate.js
+++ src/swiper-animate.js
@@ -1,63 +1,58 @@
 window.SwiperAnimate = class SwiperAnimate {
 
   constructor() {
-    this.__handlerArr = []
+    this._handlerArr = []
   }
-
 
   swiperAnimateCache() {
-    var allBoxes = window.document.documentElement.querySelectorAll(".ani");
-    for (var i = 0; i < allBoxes.length; i++) {
-      if (allBoxes[i].attributes["style"]) {
-        allBoxes[i].setAttribute("swiper-animate-style-cache", allBoxes[i].attributes["style"].value);
-      } else {
-        allBoxes[i].setAttribute("swiper-animate-style-cache", " ");
-      }
-      allBoxes[i].style.visibility = "hidden";
+    let allBoxes = window.document.documentElement.querySelectorAll(".ani")
+    for (let i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].attributes["style"] ? allBoxes[i].setAttribute("swiper-animate-style-cache", allBoxes[i].attributes["style"].value) : allBoxes[i].setAttribute("swiper-animate-style-cache", " ")
+      allBoxes[i].style.visibility = "hidden"
     }
-  }
-
-  _addAniStyle(ele, effect, duration, delay, iterationCount) {
-    var style = '';
-    ele.style.visibility = "visible";
-    ele.className = ele.className + " " + effect + " " + "animated";
-    style = ele.attributes["style"].value;
-    if (duration) {
-      style = style + "animation-duration:" + duration + ";-webkit-animation-duration:" + duration + ";";
-    }
-    if (delay) {
-      style = style + "animation-delay:" + delay + ";-webkit-animation-delay:" + delay + ";";
-    }
-    if (iterationCount) {
-      style = style + "animation-iteration-count:" + iterationCount + ";-webkit-animation-iteration-count:" + iterationCount + ";";
-    }
-    ele.setAttribute("style", style);
   }
 
   swiperAnimate(swiper) {
     this._clearSwiperAnimate();
-    var element = swiper.slides[swiper.activeIndex].querySelectorAll(".ani");
-    for (var i = 0; i < element.length; i++) {
-      var effect, duration, delay, iterationCount;
-      effect = element[i].attributes["swiper-animate-effect"] ? element[i].attributes["swiper-animate-effect"].value : "";
-      duration = element[i].attributes["swiper-animate-duration"] ? element[i].attributes["swiper-animate-duration"].value : "";
-      delay = element[i].attributes["swiper-animate-delay"] ? element[i].attributes["swiper-animate-delay"].value : "";
-      iterationCount = element[i].attributes["swiper-animation-iteration-count"] ? element[i].attributes["swiper-animation-iteration-count"].value : "1";
+    let element = swiper.slides[swiper.activeIndex].querySelectorAll(".ani")
+    for (let i = 0; i < element.length; i++) {
+      let effect, duration, delay, iterationCount
+      effect = element[i].attributes["swiper-animate-effect"] ? element[i].attributes["swiper-animate-effect"].value : ""
+      duration = element[i].attributes["swiper-animate-duration"] ? element[i].attributes["swiper-animate-duration"].value : ""
+      delay = element[i].attributes["swiper-animate-delay"] ? element[i].attributes["swiper-animate-delay"].value : ""
+      iterationCount = element[i].attributes["swiper-animation-iteration-count"] ? element[i].attributes["swiper-animation-iteration-count"].value : "1"
 
-      var aniVector = this._aniVectorGenerator(effect, duration, delay, iterationCount);
+      let aniVector = this._aniVectorGenerator(effect, duration, delay, iterationCount)
 
-      for (var j = 0; j < aniVector.length; j++) {
-        let self = this;
+      let self = this
+      for (let j = 0; j < aniVector.length; j++) {
         (function (i, j, aniVector) {
-          var handler = setTimeout(function () {
+          let handler = setTimeout(function () {
             self._clearEleAnimate(element[i], j > 0 ? aniVector[j - 1].effect : '');
-            self._addAniStyle(element[i], aniVector[j].effect, aniVector[j].duration, 0, aniVector[j].iterationCount);
-          }, aniVector[j].delay);
-          self.__handlerArr.push(handler);
-        })(i, j, aniVector);
+            self._addAniStyle(element[i], aniVector[j].effect, aniVector[j].duration, 0, aniVector[j].iterationCount)
+          }, aniVector[j].delay)
+          self._handlerArr.push(handler)
+        })(i, j, aniVector)
       }
 
     }
+  }
+
+  _addAniStyle(ele, effect, duration, delay, iterationCount) {
+    let style = ''
+    ele.style.visibility = "visible"
+    ele.className = ele.className + " " + effect + " " + "animated"
+    style = ele.attributes["style"].value
+    if (duration) {
+      style = style + "animation-duration:" + duration + ";-webkit-animation-duration:" + duration + ";"
+    }
+    if (delay) {
+      style = style + "animation-delay:" + delay + ";-webkit-animation-delay:" + delay + ";"
+    }
+    if (iterationCount) {
+      style = style + "animation-iteration-count:" + iterationCount + ";-webkit-animation-iteration-count:" + iterationCount + ";"
+    }
+    ele.setAttribute("style", style)
   }
 
   /**
@@ -80,55 +75,55 @@ window.SwiperAnimate = class SwiperAnimate {
   }]
   */
   _aniVectorGenerator(effect, duration, delay, iterationCount) {
-    effect = effect.replace(/\s/g, "").split(",");
-    duration = duration.replace(/\s/g, "").split(",");
-    delay = delay.replace(/\s/g, "").split(",");
-    iterationCount = iterationCount.replace(/\s/g, "").split(",");
+    effect = effect.replace(/\s/g, "").split(",")
+    duration = duration.replace(/\s/g, "").split(",")
+    delay = delay.replace(/\s/g, "").split(",")
+    iterationCount = iterationCount.replace(/\s/g, "").split(",")
 
-    var vector = [], delaySum = 0;
-    for (var i = 0; i < effect.length; i++) {
+    let vector = [], delaySum = 0
+    for (let i = 0; i < effect.length; i++) {
       if (i === 0) {
-        delaySum = parseFloat(delay[i]);
+        delaySum = parseFloat(delay[i])
       } else {
-        delaySum = delaySum + parseFloat(duration[i - 1]) * (iterationCount[i - 1] === 'infinite' ? 999999 : parseInt(iterationCount[i - 1])) + parseFloat(delay[i]);
+        delaySum = delaySum + parseFloat(duration[i - 1]) * (iterationCount[i - 1] === 'infinite' ? 999999 : parseInt(iterationCount[i - 1])) + parseFloat(delay[i])
       }
       vector.push({
         effect: effect[i],
         duration: duration[i],
         delay: delaySum * 1000,
         iterationCount: iterationCount[i]
-      });
+      })
     }
-    return vector;
+    return vector
   }
 
   _clearEleAnimate(ele, effect) {
     if (ele.attributes["swiper-animate-style-cache"]) {
-      ele.setAttribute("style", ele.attributes["swiper-animate-style-cache"].value);
+      ele.setAttribute("style", ele.attributes["swiper-animate-style-cache"].value)
     }
-    ele.style.visibility = "hidden";
-    ele.className = ele.className.replace("animated", "");
+    ele.style.visibility = "hidden"
+    ele.className = ele.className.replace("animated", "")
     if (effect) {
-      ele.className = ele.className.replace(effect, "");
+      ele.className = ele.className.replace(effect, "")
     }
   }
 
   _clearSwiperAnimate() {
-    for (var i = 0; i < this.__handlerArr.length; i++) {
-      clearTimeout(this.__handlerArr[i]);
+    for (let i = 0; i < this._handlerArr.length; i++) {
+      clearTimeout(this._handlerArr[i]);
     }
-    var allBoxes = window.document.documentElement.querySelectorAll(".ani");
-    for (var i = 0; i < allBoxes.length; i++) {
+    let allBoxes = window.document.documentElement.querySelectorAll(".ani")
+    for (let i = 0; i < allBoxes.length; i++) {
       if (allBoxes[i].attributes["swiper-animate-style-cache"]) {
-        allBoxes[i].setAttribute("style", allBoxes[i].attributes["swiper-animate-style-cache"].value);
+        allBoxes[i].setAttribute("style", allBoxes[i].attributes["swiper-animate-style-cache"].value)
       }
-      allBoxes[i].style.visibility = "hidden";
-      allBoxes[i].className = allBoxes[i].className.replace("animated", "");
+      allBoxes[i].style.visibility = "hidden"
+      allBoxes[i].className = allBoxes[i].className.replace("animated", "")
       if (allBoxes[i].attributes["swiper-animate-effect"]) {
-        var effect = allBoxes[i].attributes["swiper-animate-effect"].value;
-        effect = effect.replace(/\s/g, "").split(",");
-        for (var j = 0; j < effect.length; j++) {
-          allBoxes[i].className = allBoxes[i].className.replace(effect[j], "");
+        let effect = allBoxes[i].attributes["swiper-animate-effect"].value
+        effect = effect.replace(/\s/g, "").split(",")
+        for (let j = 0; j < effect.length; j++) {
+          allBoxes[i].className = allBoxes[i].className.replace(effect[j], "")
         }
       }
     }
